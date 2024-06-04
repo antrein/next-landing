39a42b93180ecc2a5654ae575773aebe3f64acfb
tiansibatuara <sibatuarachristian@gmail.com>
2024-06-04 18:03:25 +0700

    Refactor Hero component styles for responsive design

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section
       className="max-container padding-container flex flex-col
-    gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row"
+     py-10 pb-32 lg:py-20 xl:flex-row"
     >
       <div className="hero-map" />
 
